4ed5217ebc8a859e2ece57dcdfae738b07ff0821
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-05-01 15:14:56 -0500

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,25 @@
 class DogsController < ApplicationController
+
+
+    before_action :current_dog, only: [:show]
+
+    def index
+        #byebug
+        @dogs = Dog.all.sort_by do |dog|
+            dog.employees.uniq.count
+        end
+
+    end
+
+    def show
+        
+    end
+
+    private
+
+    def current_dog
+        @dog = Dog.find(params[:id])
+    end
+
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,49 @@
 class EmployeesController < ApplicationController
+
+    before_action :current_employee, only: [:show, :edit, :update, :destroy]
+
+    def index
+        #byebug
+        @employees = Employee.all
+    end
+
+    def show
+        
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to @employee
+    end
+
+    def edit
+
+    end
+
+    def update
+        @employee.update(employee_params)
+        redirect_to @employee
+    end
+
+    def destroy
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    private
+
+    def current_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
+
+
 end
